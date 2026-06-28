5b92d01c4384abe913a5f2ac40e387b50043cb8d
MkD08 <doumams2103@gmail.com>
2026-06-28 08:24:03 +0000

    push notif2

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,32 @@ importScripts("https://www.gstatic.com/firebasejs/12.0.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/12.0.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCVLKijqB9S3n2xkGrDSn5mos1MT8WCfrM",
+  apiKey: "AIzaSyCVLKijqB9S3n2xkGrDSn5mos1MT8WCfrM", // pas la vraie
   authDomain: "thielal.firebaseapp.com",
   projectId: "thielal",
   storageBucket: "thielal.firebasestorage.app",
   messagingSenderId: "220814183760",
-  appId: "1:220814183760:web:9bd4bff4af59831272f055",
+  appId: "1:220814183760:web:9bd4bff4af59831272f055", // pas la vraie
 });
+
+// ===== AJOUTER ICI =====
+console.log("🔥 Firebase Service Worker chargé");
+
+self.addEventListener("push", (event) => {
+    console.log("🔥 PUSH EVENT :", event);
+  
+    event.waitUntil(
+      self.registration.showNotification("TEST PUSH", {
+        body: "Le push est bien arrivé.",
+        icon: "/logo.png",
+      })
+    );
+  });;
+
+self.addEventListener("notificationclick", (event) => {
+  console.log("🔥 Notification cliquée :", event);
+});
+// =======================
 
 const messaging = firebase.messaging();
 
@@ -20,8 +39,8 @@ messaging.onBackgroundMessage((payload) => {
 
   const notificationOptions = {
     body: payload.notification?.body || "",
-    icon: "/logo.png", // À adapter selon votre projet
-    badge: "/logo.png", // À adapter selon votre projet
+    icon: "/logo.png",
+    badge: "/logo.png",
   };
 
   self.registration.showNotification(
